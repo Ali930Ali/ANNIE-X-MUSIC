ad61d86d8151f365bcfadf7eab944842b0cfe61f
Ali930Ali <alibeff48@gmail.com>
2024-06-23 21:50:59 +0400

    Update language.py

--- ANNIEMUSIC/utils/decorators/language.py
+++ ANNIEMUSIC/utils/decorators/language.py
@@ -8,7 +8,7 @@ def language(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    text=f"{app.mention} Sistem bakımdaymış, ziyaret edebilirsin. <a href={SUPPORT_CHAT}>Destek sohbeti.</a> Nedeni öğrenmek için..",
                     disable_web_page_preview=True,
                 )
         try:
@@ -20,7 +20,7 @@ def language(mystic):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("tr")
         return await mystic(_, message, language)
 
     return wrapper
@@ -31,14 +31,14 @@ def languageCB(mystic):
         if await is_maintenance() is False:
             if CallbackQuery.from_user.id not in SUDOERS:
                 return await CallbackQuery.answer(
-                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    f"{app.mention} Sistem bakımdaymış. Nedenini öğrenmek için destek sohbetini ziyaret edebilirsin..",
                     show_alert=True,
                 )
         try:
             language = await get_lang(CallbackQuery.message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("tr")
         return await mystic(_, CallbackQuery, language)
 
     return wrapper
@@ -50,7 +50,7 @@ def LanguageStart(mystic):
             language = await get_lang(message.chat.id)
             language = get_string(language)
         except:
-            language = get_string("en")
+            language = get_string("tr")
         return await mystic(_, message, language)
 
     return wrapper
