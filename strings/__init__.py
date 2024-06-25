44ee5974f9dafe5e93f7e00d1dd899eb576b42ed
Ali930Ali <alibeff48@gmail.com>
2024-06-25 21:46:16 +0400

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -14,9 +14,9 @@ def get_string(lang: str):
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
+            open(r"./strings/langs/tr.yml", encoding="utf8")
         )
-        languages_present["en"] = languages["en"]["name"]
+        languages_present["tr"] = languages["tr"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         if language_name == "en":
@@ -24,11 +24,11 @@ for filename in os.listdir(r"./strings/langs/"):
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        for item in languages["en"]:
+        for item in languages["tr"]:
             if item not in languages[language_name]:
                 languages[language_name][item] = languages["en"][item]
     try:
         languages_present[language_name] = languages[language_name]["name"]
     except:
-        print("There is some issue with the language file inside bot.")
+        print("Bot içinde dil dosyasında bir sorun var.")
         exit()
