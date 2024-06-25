6022dc48dc1d0a075ae52f07917b4ee4f9e999e1
Ali930Ali <alibeff48@gmail.com>
2024-06-25 20:47:00 +0400

    Update help.py

--- ANNIEMUSIC/utils/inline/help.py
+++ ANNIEMUSIC/utils/inline/help.py
@@ -221,7 +221,7 @@ def private_help_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="💗 ʜᴇʟᴘ 💗",
+                text="💗 Yardım 💗",
                 callback_data="settings_back_helper",
             ),
         ],
